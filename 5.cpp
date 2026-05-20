2ab654cb2af1e6a857cfdbd62f90a45662291733
Ayandip <137981575+Ayandip244@users.noreply.github.com>
2026-05-20 08:58:27 +0530

    Ansuman and the Security Alarm Codes

---
+++ 5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+  int n; cin>>n;
+  vector<int> a(n);
+  for(int i=0;i<n;i++) cin>>a[i];
+  for(int i=1;i<n;i++){
+    if(a[i] == a[i-1]) return cout<<"INVALID "<<i,0;
+  }
+  cout<<"VALID";
+}
